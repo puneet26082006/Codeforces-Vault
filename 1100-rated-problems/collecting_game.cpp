0f1e8b0b9f9f6c82b353f75a14bcb158ceea468d
Puneet Saxena <puneetsaxena23@outlook.com>
2026-09-24 22:06:01 +0530

    1100 rated

---
+++ 1100-rated-problems/collecting_game.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+#define fr(i,n) for (ll i=0;i<n;i++)
+#define pb push_back
+#define sz(x) (int)x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+// typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+/*
+**************************************************************************************************
+**************************************************************************************************
+
+
+                    ***        **************
+                    ***        **************
+                    ***  ***   ***   ***
+                    ***  ***   ***   ***
+                    ***        ***
+                    *************************
+                    *************************
+                               ***        ***
+                         ***   ***   ***  ***
+                         ***   ***   ***  ***
+                    **************        ***
+                    **************        ***
+
+**************************************************************************************************
+**************************************************************************************************
+*/
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+#pragma GCC optimize("unroll-loops,O3,Ofast") //even 10^8+ also works with this
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+
+int knightx[8] = { -1, -2, -2, -1, 1, 2, 2, 1};
+int knighty[8] = { -2, -1, 1, 2, 2, 1, -1, -2};
+
+
+
+
+
+void solve() {
+    ll n ;
+    cin>> n ;
+
+    vector<pair<ll, ll>> a(n) ;
+    for(ll i = 0 ; i < n ; i++){
+        ll x ;
+        cin>> x ;
+
+        a[i] = {x, i} ;
+    }
+
+    sort(a.begin(), a.end()) ;
+
+    vector<ll> pref(n);
+    pref[0] = a[0].ff ;
+    for(ll i = 1 ; i < n ; i++){
+        pref[i] = pref[i - 1] + a[i].ff ;
+    }
+
+    vector<ll> ans(n) ;
+    ans[a[n - 1].ss] = n - 1 ;
+    for(ll i = n - 2 ; i >= 0 ; i--){
+        if(pref[i] >= a[i + 1].ff){
+            ans[a[i].ss] = ans[a[i + 1].ss] ;
+        } else {
+            ans[a[i].ss] = i ;
+        }
+    }
+
+    for(auto &it : ans) cout<< it <<" ";
+    cout<<endl ;
+
+
+}
+
+
+
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
+
+
